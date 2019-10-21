b4848d3689441f972899849a013b4c9b9caf0bed
mrzahidali <2018cs136@student.uet.edu.pk>
2019-10-21 21:45:29 +0500

    variable

--- test.cpp
+++ test.cpp
@@ -2,12 +2,12 @@
 #include <conio.h>
 int main()
 {
-	int l=1, m, n;
-	while (l != 0)
+	int q=1, m, n;
+	while (q != 0)
 	{
 		cout << "\n\n\tinput pattren wise\t\t******INPUT 0 to exit*****" << endl;
-		cin >> l >> m >> n;
-		date2019 c(m, l);
+		cin >> q >> m >> n;
+		date2019 c(m, q);
 		cout << n << "days after";
 		c.print();
 		cout << "is";
